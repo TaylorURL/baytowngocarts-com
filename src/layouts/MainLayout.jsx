8f4c8fc0758a605019c850b7e4acff7a5bbf8142
Sunday <trenton.taylor.email@gmail.com>
2026-06-15 10:44:26 -0500

    sunday: Edit src/layouts/MainLayout.jsx

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Outlet, useLocation } from "react-router-dom";
 import Header from "../components/common/Header.jsx";
 import Footer from "../components/common/Footer.jsx";
