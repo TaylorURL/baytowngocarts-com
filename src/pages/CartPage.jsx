79604012e383ad870993963d713fb12398b1d3f5
Sunday <trenton.taylor.email@gmail.com>
2026-06-15 12:19:28 -0500

    sunday: Edit src/pages/CartPage.jsx

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -297,7 +297,7 @@ export default function CartPage() {
         minHeightClass="min-h-[40vh]"
         dividerColorClass="bg-white"
       />
-      <section className="py-24 bg-gradient-to-br from-gray-50 to-white">
+      <section className="py-24 bg-asphalt-50">
         <div className="container mx-auto px-4 sm:px-6 lg:px-8">
           <div className="max-w-4xl mx-auto">
             <button
